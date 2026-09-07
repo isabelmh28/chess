e65300799695cba2b87880573e6d5d74c8072137
isabelmh28 <isabelmh@byu.edu>
2026-09-07 15:09:05 -0600

    ChessPosition Updates. Added hashCode and equals methods. Added row and int private members and updated getter functions.

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
@@ -8,22 +10,39 @@ package chess;
  */
 public class ChessPosition {
 
+    private final int row;
+    private final int col;
+
     public ChessPosition(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 
     /**
      * @return which row this position is in
      * 1 codes for the bottom row
      */
-    public int getRow() {
-        throw new RuntimeException("Not implemented");
-    }
+    public int getRow() { return row;}
 
     /**
      * @return which column this position is in
      * 1 codes for the left column
      */
-    public int getColumn() {
-        throw new RuntimeException("Not implemented");
+    public int getColumn() { return col;}
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // Check reference equality
+        if(this == obj) return true;
+        // Check null and see if Classes match
+        if((obj == null) || (getClass() != obj.getClass())) return false;
+        // Cast and Compare data
+        ChessPosition that = (ChessPosition) obj;
+        return ((row == that.row) && (col == that.col));
     }
 }
