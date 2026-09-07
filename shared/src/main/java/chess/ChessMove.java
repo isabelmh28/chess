0fb3e243f2f095e18688c8089c91e62e31c47a46
isabelmh28 <isabelmh@byu.edu>
2026-09-07 15:52:50 -0600

    ChessMove Module updates. Private members for start position, end position, and the promotion type in the case of a promotion added. Getter functions updated. The equals and hashCode methods overridden.

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,4 +1,12 @@
+/**
+ * ChessMove Module
+ * Represents the chess piece's move, including start/end position.
+ * Author: Isabel Hinton off Template
+ */
+
 package chess;
+
+import java.util.Objects;
 
 /**
  * Represents moving a chess piece on a chessboard
@@ -8,23 +16,26 @@ package chess;
  */
 public class ChessMove {
 
+    private final ChessPosition start;
+    private final ChessPosition end;
+    private final ChessPiece.PieceType promoteType;
+
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
+        start = startPosition;
+        end = endPosition;
+        promoteType = promotionPiece;
     }
 
     /**
      * @return ChessPosition of starting location
      */
-    public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
-    }
+    public ChessPosition getStartPosition() { return start;}
 
     /**
      * @return ChessPosition of ending location
      */
-    public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
-    }
+    public ChessPosition getEndPosition() { return end;}
 
     /**
      * Gets the type of piece to promote a pawn to if pawn promotion is part of this
@@ -32,7 +43,21 @@ public class ChessMove {
      *
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
-    public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+    public ChessPiece.PieceType getPromotionPiece() { return promoteType;}
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(start, end, promoteType);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // Check reference equality
+        if(this == obj) return true;
+        // Check null and see if Classes match
+        if((obj == null) || (getClass() != obj.getClass())) return false;
+        // Cast and Compare data
+        ChessMove that = (ChessMove) obj;
+        return ((start == that.start) && (end == that.end) && (promoteType == that.promoteType));
     }
 }
