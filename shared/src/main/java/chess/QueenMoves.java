330960176de826b5e2a42e2ba0f4497e45eb2d57
isabelmh28 <isabelmh@byu.edu>
2026-09-11 23:40:28 -0600

    Phase 0: Moves. QueenMoves calculator to return a list of moves for the Queen piece.

---
+++ shared/src/main/java/chess/QueenMoves.java
@@ -0,0 +1,33 @@
+/**
+ * QueenMoves Module
+ * Calculates the available moves for a Queen
+ * Author: Isabel Hinton
+ */
+
+package chess;
+
+import java.util.Collection;
+import java.util.ArrayList;
+
+public class QueenMoves {
+    public static Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor myColor) {
+        Collection<ChessMove> queenMoves = new ArrayList<>();
+        int[][] directions = {
+                // Straights
+                {0, -1},  // Down
+                {0, 1},   // Up
+                {-1, 0},  // Left
+                {1, 0},   // Right
+                // Diagonals
+                {-1, -1}, // Down Left
+                {-1, 1},  // Up Left
+                {1, 1},   // Up Right
+                {1, -1}   // Down Right
+        };
+        // Add moves to Collection per direction
+        for(int[] currDirection : directions) {
+            queenMoves.addAll(ChessPiece.moveRay(board, myPosition, myColor, currDirection[0], currDirection[1]));
+        }
+        return queenMoves;
+    }
+}
